c72425c04767a84ad24d29dc78a54b6111e33815
Rjakubiec <jakubiecrafal@gmail.com>
2016-04-09 18:08:09 +0200

    p

--- budva.js
+++ budva.js
@@ -3,13 +3,14 @@
 Budva.controller('BudvaCtrl', function($scope, $fblogin) {
 
     $scope.login = function() {
-console.log('test');
+        console.log('test');
         $fblogin({
-            
-            fbId: '222660488094320',
-            permissions: 'email,user_birthday',
-            success: function(data) {             
-                console.log('User birthday' + data.birthday + 'and email ' + data.email);
+            fbId: '{FB app id}',
+            success: function(data) {
+                console.log('Basic public user data returned by Facebook', data);
+            },
+            error: function(error) {
+                console.log('An error occurred.', error);
             }
         });
     };
